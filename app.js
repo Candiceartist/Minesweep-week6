5a62cd22c2a017189ba235ecd3d4491215ab2d0a
Candice Jovon Juarez <candice.artist@gmail.com>
2023-02-21 14:11:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// start active tiles active bombs alive or dead text inside
+// display with advent AudioListener
+// start game and place bombs randomly append to display
+// fucntion to create table 
+// place single bomb 
+// hit bomb "you loose"
+// // if number is next to bomb flag function 
+// all flags placed correctly "you win"
+// reset to first funtion
+// repeat for player two 
