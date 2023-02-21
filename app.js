52d46f5ebfb53c001859e5b503fa3aa6aded6457
Candice Jovon Juarez <candice.artist@gmail.com>
2023-02-21 15:12:39 -0500

    added event listener when page loads

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-// start active tiles active bombs alive or dead text inside
-// display with advent AudioListener
+// create mine field start active tiles active bombs alive or dead text inside
+const mineFields = {
+   rows:3,
+   collumns:6,
+   bombs: 5,
+   bomb: "&#x1F4A3",
+   alive: "true",
+   colors: {1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'}
+}
+
+// display with advent AudioListener to start game 
+window.addEventListener('load', function() {
+    document.getElementsByClassName('lost').style.display="none";
+    startGame();
+});
+// when game is lost display message
+function startGame() {
+    components.bombs = placeBombs();
+    document.getElementsByClassName('mineField').appendChild(createTable());
+}
 // start game and place bombs randomly append to display
 // fucntion to create table 
 // place single bomb 
